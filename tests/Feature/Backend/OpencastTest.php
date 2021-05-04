12179bffb4840e973767501fd74255fac22902db
Stefanos Georgopoulos <stefanos.georgopoulos@fau.de>
2021-05-04 17:07:48 +0200

    Mock Opencast Service to OpencastController test
    Signed-off-by: Stefanos Georgopoulos <stefanos.georgopoulos@fau.de>

--- tests/Feature/Backend/OpencastTest.php
+++ tests/Feature/Backend/OpencastTest.php
@@ -2,18 +2,36 @@
 
 namespace Tests\Feature\Backend;
 
+use App\Services\OpencastService;
+use GuzzleHttp\Handler\MockHandler;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Tests\Setup\WorksWithOpencastClient;
 use Tests\TestCase;
 
 class OpencastTest extends TestCase
 {
-    use RefreshDatabase, WithFaker;
+    use RefreshDatabase, WithFaker, WorksWithOpencastClient;
+
+    private OpencastService $opencastService;
+
+    private MockHandler $mockHandler;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->mockHandler  = $this->swapOpencastClient();
+
+        $this->opencastService = app(OpencastService::class);
+    }
 
     /** @test */
     public function it_should_check_for_opencast_status(): void
     {
         $this->signIn();
+
+        $this->mockHandler->append($this->mockHealthResponse());
 
         $this->get(route('opencast'))->assertStatus(200);
     }
