bbe9c1ce87b677a1624e507f5018af320ea1158d
Stefanos Georgopoulos <stefanos.georgopoulos@fau.de>
2022-02-23 15:44:39 +0100

    Update composer packages
    Signed-off-by: Stefanos Georgopoulos <stefanos.georgopoulos@fau.de>

--- app/Enums/Content.php
+++ app/Enums/Content.php
@@ -6,17 +6,17 @@ use Illuminate\Support\Str;
 
 enum Content: string
 {
-    case Presenter = "presenter";
-    case Presentation = "presentation";
-    case Composite = "composite";
-    case Audio = 'audio';
-    case Slides = 'slides';
-    case Cc = 'cc';
-    case Smil = 'smil';
+case Presenter = "presenter";
+case Presentation = "presentation";
+case Composite = "composite";
+case Audio = 'audio';
+case Slides = 'slides';
+case Cc = 'cc';
+case Smil = 'smil';
 
     //lowercase state's name
     public function lower(): string
     {
         return Str::lower($this->name);
     }
-}
+    }
