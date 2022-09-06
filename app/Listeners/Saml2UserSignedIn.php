233ce5c09d73228ec8481c0867bd4f573c8e580e
Stefanos Georgopoulos <stefanos.georgopoulos@fau.de>
2022-09-06 11:43:28 +0200

    Saml2 fixes
    Signed-off-by: Stefanos Georgopoulos <stefanos.georgopoulos@fau.de>

--- app/Listeners/Saml2UserSignedIn.php
+++ app/Listeners/Saml2UserSignedIn.php
@@ -21,44 +21,42 @@ class Saml2UserSignedIn
     /**
      * Handle the event.
      *
-     * @param  SignedIn  $event
+     * @param SignedIn $event
      * @return void
      */
     public function handle(SignedIn $event): void
     {
-        Event::listen(SignedIn::class, function (SignedIn $event) {
-            $messageId = $event->getAuth()->getLastMessageId();
+        $messageId = $event->getAuth()->getLastMessageId();
 
-            // your own code preventing reuse of a $messageId to stop replay attacks
-            $samlUser = $event->getSaml2User();
+        // your own code preventing reuse of a $messageId to stop replay attacks
+        $samlUser = $event->getSaml2User();
 
-            $samlUser = [
-                'id' => $samlUser->getUserId(),
-                'attributes' => $samlUser->getAttributes(),
-                'sessionIndex' => $samlUser->getSessionIndex(),
-                'nameId' => $samlUser->getNameId(),
-            ];
+        $samlUser = [
+            'id'           => $samlUser->getUserId(),
+            'attributes'   => $samlUser->getAttributes(),
+            'sessionIndex' => $samlUser->getSessionIndex(),
+            'nameId'       => $samlUser->getNameId(),
+        ];
 
-            Log::info($samlUser);
-            //check if email already exists and fetch user
-            $user = User::firstOrCreate(
-                [
-                    'username' => $samlUser['attributes']['urn:mace:dir:attribute-def:uid'][0],
-                ],
-                [
-                    'username' => $samlUser['attributes']['urn:mace:dir:attribute-def:uid'][0],
-                    'email' => $samlUser['attributes']['urn:mace:dir:attribute-def:mail'][0],
-                    'password' => bcrypt(str()->random(40)),
-                    'first_name' => str($samlUser['attributes']['urn:mace:dir:attribute-def:displayName'][0])->before(' '),
-                    'last_name' => str($samlUser['attributes']['urn:mace:dir:attribute-def:displayName'][0])->after(' '),
-                ]
-            );
-            //insert sessionIndex and nameId into session
-            session(['sessionIndex' => $samlUser['sessionIndex']]);
-            session(['nameId' => $samlUser['nameId']]);
+        Log::info($samlUser);
+        //check if email already exists and fetch user
+        $user = User::firstOrCreate(
+            [
+                'username' => $samlUser['attributes']['urn:mace:dir:attribute-def:uid'][0],
+            ],
+            [
+                'username'   => $samlUser['attributes']['urn:mace:dir:attribute-def:uid'][0],
+                'email'      => $samlUser['attributes']['urn:mace:dir:attribute-def:mail'][0],
+                'password'   => bcrypt(str()->random(40)),
+                'first_name' => str($samlUser['attributes']['urn:mace:dir:attribute-def:displayName'][0])->before(' '),
+                'last_name'  => str($samlUser['attributes']['urn:mace:dir:attribute-def:displayName'][0])->after(' '),
+            ]
+        );
+        //insert sessionIndex and nameId into session
+        session(['sessionIndex' => $samlUser['sessionIndex']]);
+        session(['nameId' => $samlUser['nameId']]);
 
-            // Login a user.
-            Auth::login($user);
-        });
+        // Login a user.
+        Auth::login($user);
     }
 }
