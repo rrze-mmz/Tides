2eb528e5ee29972972ce6a1c267238505fc60684
Stefanos Georgopoulos <stefanos.georgopoulos@fau.de>
2024-06-13 13:53:34 +0200

    Update npm and set up vite chunksizewarning
    Signed-off-by: Stefanos Georgopoulos <stefanos.georgopoulos@fau.de>

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
   plugins: [laravel(['resources/css/app.css', 'resources/js/app.js'])],
+  build: { chunkSizeWarningLimit: 1600 },
   resolve: {
     alias: {
       '@': '/resources/js',
